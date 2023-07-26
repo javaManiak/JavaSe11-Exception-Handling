f0954860768c3bf5f5e41109eb198ebe3e43c6a8
Ben Ma <bennma2000@gmail.com>
2023-07-25 21:40:48 -0500

    m3 commit

--- src/main/java/com/exceptions/m3/TryWithResourcesExample.java
+++ src/main/java/com/exceptions/m3/TryWithResourcesExample.java
@@ -4,12 +4,13 @@ import java.io.*;
 
 public class TryWithResourcesExample {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         try (FileInputStream in = new FileInputStream("in.txt");
                 FileOutputStream out = new FileOutputStream("out.txt")) {
             out.write(in.readAllBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
     }
 }
--- src/main/java/com/exceptions/m3/TryWithResourcesReverseClose.java
+++ src/main/java/com/exceptions/m3/TryWithResourcesReverseClose.java
@@ -1,5 +1,7 @@
 package com.exceptions.m3;
 
+import java.io.IOException;
+
 public class TryWithResourcesReverseClose {
 
     public static void main(String[] args) {
